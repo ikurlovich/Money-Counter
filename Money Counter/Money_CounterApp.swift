bfa63d1420365ef4b66ced640607aef7e9ef1887
ikurlovich <ikurlovich@icloud.com>
2023-06-02 22:28:36 +0400

    Initial Commit

---
+++ Money Counter/Money_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Money_CounterApp.swift
+//  Money Counter
+//
+//  Created by Илья Курлович on 02.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Money_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
